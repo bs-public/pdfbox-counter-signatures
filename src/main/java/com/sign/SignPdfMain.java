3da150b793c63f0107baddb5d4a62f0ecdb94c0d
bs <26665808+bs-public@users.noreply.github.com>
2026-03-02 17:13:59 +0530

    Update signing to add visual appearance

--- src/main/java/com/sign/SignPdfMain.java
+++ src/main/java/com/sign/SignPdfMain.java
@@ -2,18 +2,33 @@ package com.sign;
 
 import com.util.KeyStoreUtil;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationWidget;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceDictionary;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceStream;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.PDSignature;
+import org.apache.pdfbox.pdmodel.interactive.digitalsignature.SignatureOptions;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDSignatureField;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.Security;
 import java.security.cert.Certificate;
 import java.util.Calendar;
+import java.util.Date;
 
 public class SignPdfMain {
 
@@ -63,9 +78,78 @@ public class SignPdfMain {
       signature.setReason("Test");
       signature.setSignDate(Calendar.getInstance());
 
-      document.addSignature(signature, cmsService);
+      int pageIndex = 0;
+      float x = 50;
+      float y = 600;
+      float width = 200;
+      float height = 75;
+
+      PDRectangle rect = new PDRectangle(x, y, width, height);
+
+      SignatureOptions signatureOptions = new SignatureOptions();
+      signatureOptions.setVisualSignature(createVisualSignatureTemplate(document, pageIndex, rect));
+      signatureOptions.setPage(pageIndex);
+
+      document.addSignature(signature, cmsService, signatureOptions);
 
       document.saveIncremental(fos);
     }
+  }
+
+  private static InputStream createVisualSignatureTemplate(
+      PDDocument srcDoc, int pageIndex, PDRectangle rect) throws Exception {
+
+    PDDocument doc = new PDDocument();
+
+    PDPage page = new PDPage(srcDoc.getPage(pageIndex).getMediaBox());
+    doc.addPage(page);
+
+    PDAcroForm acroForm = new PDAcroForm(doc);
+    doc.getDocumentCatalog().setAcroForm(acroForm);
+
+    PDSignatureField signatureField = new PDSignatureField(acroForm);
+    PDAnnotationWidget widget = signatureField.getWidgets().get(0);
+
+    widget.setRectangle(rect);
+    widget.setPage(page);
+    page.getAnnotations().add(widget);
+
+    acroForm.getFields().add(signatureField);
+
+    PDAppearanceDictionary appearance = new PDAppearanceDictionary();
+    PDAppearanceStream appearanceStream = new PDAppearanceStream(doc);
+
+    appearanceStream.setResources(new PDResources());
+    appearanceStream.setBBox(new PDRectangle(width(rect), height(rect)));
+
+    try (PDPageContentStream cs = new PDPageContentStream(doc, appearanceStream)) {
+
+      cs.beginText();
+      cs.setFont(PDType1Font.HELVETICA, 10);
+      cs.newLineAtOffset(10, height(rect) - 20);
+      cs.showText("Digitally Signed by Signer A");
+      cs.newLineAtOffset(0, -15);
+      cs.showText("Reason: Test");
+      cs.newLineAtOffset(0, -15);
+      cs.showText("Date: " + new Date());
+      cs.endText();
+    }
+
+    appearance.setNormalAppearance(appearanceStream);
+    widget.setAppearance(appearance);
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    doc.save(baos);
+    doc.close();
+
+    return new ByteArrayInputStream(baos.toByteArray());
+  }
+
+  private static float width(PDRectangle rect) {
+    return rect.getWidth();
+  }
+
+  private static float height(PDRectangle rect) {
+    return rect.getHeight();
   }
 }
